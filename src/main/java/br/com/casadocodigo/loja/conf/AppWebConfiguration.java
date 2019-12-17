9e3bdb407bcfa5807ad665fc89452c41663b8c02
eu-vinnom <vinnom.2d@gmail.com>
2019-12-17 16:49:30 -0300

    adição do extendMessageConverters para o pretty print do json

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -17,6 +17,8 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.accept.ContentNegotiationManager;
@@ -25,10 +27,7 @@ import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
@@ -44,99 +43,110 @@ import br.com.casadocodigo.loja.models.CarrinhoCompras;
 @EnableWebMvc
 @ComponentScan(basePackageClasses = { HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class })
 @EnableCaching
-@EnableAspectJAutoProxy(proxyTargetClass = true) 
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 
-	@Bean
-	public InternalResourceViewResolver internalResourceViewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
+  @Bean
+  public InternalResourceViewResolver internalResourceViewResolver() {
+    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+    resolver.setPrefix("/WEB-INF/views/");
+    resolver.setSuffix(".jsp");
 
-		// resolver.setExposeContextBeansAsAttributes(true);
-		resolver.setExposedContextBeanNames("carrinhoCompras");
+    // resolver.setExposeContextBeansAsAttributes(true);
+    resolver.setExposedContextBeanNames("carrinhoCompras");
 
-		return resolver;
-	}
+    return resolver;
+  }
 
-	@Bean
-	public MessageSource messageSource() {
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("/WEB-INF/messages");
-		messageSource.setDefaultEncoding("UTF-8");
-		messageSource.setCacheSeconds(1);
-		return messageSource;
-	}
+  @Bean
+  public MessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("/WEB-INF/messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    messageSource.setCacheSeconds(1);
+    return messageSource;
+  }
+  
+  @Bean
+  public FormattingConversionService mvcConversionService() {
+    DefaultFormattingConversionService cs = new DefaultFormattingConversionService();
+    DateFormatterRegistrar reg = new DateFormatterRegistrar();
+       
+    reg.setFormatter(new DateFormatter("yyyy-MM-dd"));
+    reg.registerFormatters(cs);
+    
+    return cs;
+  }
 
-	@Bean
-	public FormattingConversionService mvcConversionService() {
-		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
-		DateFormatterRegistrar registrar = new DateFormatterRegistrar();
-		registrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
-		registrar.registerFormatters(conversionService);
+  @Bean
+  public MultipartResolver multipartResolver() {
+    return new StandardServletMultipartResolver();
+  }
 
-		return conversionService;
-	}
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 
-	@Bean
-	public MultipartResolver multipartResolver() {
-		return new StandardServletMultipartResolver();
-	}
+  @Bean
+  public CacheManager cacheManger() {
+    CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder().maximumSize(100).expireAfterAccess(5,
+        TimeUnit.MINUTES);
+    GuavaCacheManager manager = new GuavaCacheManager();
+    manager.setCacheBuilder(builder);
+    return manager; // new ConcurrentMapCacheManager();
+  }
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+  @Bean
+  public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager) {
+    List<ViewResolver> viewResolvers = new ArrayList<>();
+    viewResolvers.add(internalResourceViewResolver());
+    viewResolvers.add(new JsonViewResolver());
 
-	@Bean
-	public CacheManager cacheManger() {
-		CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder().maximumSize(100).expireAfterAccess(5,
-				TimeUnit.MINUTES);
-		GuavaCacheManager manager = new GuavaCacheManager();
-		manager.setCacheBuilder(builder);
-		return  manager; //new ConcurrentMapCacheManager();
-	}
-	
-	@Bean
-	public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager) {
-		List<ViewResolver> viewResolvers = new ArrayList<>();
-		viewResolvers.add(internalResourceViewResolver());
-		viewResolvers.add(new JsonViewResolver());
-		
-		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
-		resolver.setViewResolvers(viewResolvers);
-		resolver.setContentNegotiationManager(manager);
-		return resolver;
-	}
+    ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+    resolver.setViewResolvers(viewResolvers);
+    resolver.setContentNegotiationManager(manager);
+    return resolver;
+  }
 
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
-	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new LocaleChangeInterceptor());
-	}
-	
-	@Bean
-	public LocaleResolver localeResolver(){
-	    return new CookieLocaleResolver();
-	}
-	
-	@Bean
-	public MailSender mailSender() {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost("seu servidor smtp");//por exemplo smtp.gmail.com
-		mailSender.setUsername("seu email");
-		mailSender.setPassword("sua senha");
-		mailSender.setPort(587);
-		
-		Properties mailProperties = new Properties();
-		mailProperties.setProperty("mail.smtp.auth", "true");
-		mailProperties.setProperty("mail.smtp.starttls.enable", "true");
-		mailSender.setJavaMailProperties(mailProperties);
-		
-		return mailSender;
-	}
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    configurer.enable();
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LocaleChangeInterceptor());
+  }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    return new CookieLocaleResolver();
+  }
+
+  @Bean
+  public MailSender mailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost("seu servidor smtp");// por exemplo smtp.gmail.com
+    mailSender.setUsername("seu email");
+    mailSender.setPassword("sua senha");
+    mailSender.setPort(587);
+
+    Properties mailProperties = new Properties();
+    mailProperties.setProperty("mail.smtp.auth", "true");
+    mailProperties.setProperty("mail.smtp.starttls.enable", "true");
+    mailSender.setJavaMailProperties(mailProperties);
+
+    return mailSender;
+  }
+
+  @Override
+  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    for (HttpMessageConverter<?> converter : converters) {
+      if (converter instanceof MappingJackson2HttpMessageConverter) {
+        MappingJackson2HttpMessageConverter jacksonConverter = (MappingJackson2HttpMessageConverter) converter;
+        jacksonConverter.setPrettyPrint(true);
+      }
+    }
+  }
 }
