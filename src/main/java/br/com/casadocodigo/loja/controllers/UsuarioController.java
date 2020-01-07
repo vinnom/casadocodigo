bf93ca884ee6bd62d7f9a0fa094e65d775101c08
eu-vinnom <vinnom.2d@gmail.com>
2020-01-07 01:42:37 -0300

    correção na configuração do InitBinder
    o InitBinder tinha sido comentado, pois durante a implementação da
    alteração de permissões, ele estava emitindo uma exception.
    
    Agora que esse implementação está funcionando corretamente, o InitBinder
    foi descomentado e as importações refeitas.

--- src/main/java/br/com/casadocodigo/loja/controllers/UsuarioController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/UsuarioController.java
@@ -8,9 +8,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -33,10 +32,10 @@ public class UsuarioController {
   @Autowired
   private UsuarioValidation validaUsuario;
 
-//  @InitBinder
-//  public void initBinder(WebDataBinder binder) {
-//    binder.addValidators(validaUsuario);
-//  }
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    binder.addValidators(validaUsuario);
+  }
 
   @RequestMapping("/form")
   public ModelAndView form(Usuario usuario) {
